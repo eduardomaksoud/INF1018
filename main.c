c57b7022357b85a90b9a6010b462b423776cc96d
Eduardo Maksoud <eduardomaksoud@gmail.com>
2019-06-13 10:14:27 -0300

    init

---
+++ main.c
@@ -0,0 +1,19 @@
+
+
+#include <stdio.h>
+typedef int (*funcp) ();
+funcp gera(FILE *f);
+
+
+int main (void) {
+  int line = 1;
+  int  c;
+  funcp func;
+  FILE *myfp;
+  if ((myfp = fopen ("simples", "r")) == NULL) {
+    perror ("nao conseguiu abrir arquivo!");
+    exit(1);
+  }
+  func = gera(myfp);
+  printf("%d \n",func());
+}
